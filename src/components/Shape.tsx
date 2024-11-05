606d282f9b6c04ed4e340d02f9f983fd72d809c2
DoctorTone <tony@drt-software.com>
2024-11-05 19:18:04 +0000

    Added more randomness for spinning shapes

--- src/components/Shape.tsx
+++ src/components/Shape.tsx
@@ -5,18 +5,30 @@ import { SCENE } from "../state/Config";
 
 const Shape = () => {
   const shapeRef = useRef(null);
+  const rotInc = Math.random() * Math.PI;
+  let direction = Math.random() - 0.5;
 
   useFrame((state, delta) => {
     const time = state.clock.getElapsedTime();
     if (shapeRef.current) {
-      shapeRef.current.position.x = 4 * Math.sin(time / 2);
-      shapeRef.current.rotation.y += delta;
+      shapeRef.current.position.x += delta * direction;
+      if (
+        shapeRef.current.position.x > SCENE.MAX_X ||
+        shapeRef.current.position.x < -SCENE.MAX_X
+      ) {
+        direction *= -1;
+      }
+      shapeRef.current.rotation.y += delta * rotInc;
     }
   });
 
   return (
-    <group>
-      <Instance ref={shapeRef} />
+    <group scale={SCENE.SCALE}>
+      <Instance
+        ref={shapeRef}
+        position={[Math.random() * 10 - 5, Math.random() * 10 - 5, 0]}
+        rotation={[Math.random() * 2, Math.random() * 2, Math.random() * 2]}
+      />
     </group>
   );
 };
--- src/state/Config.ts
+++ src/state/Config.ts
@@ -3,13 +3,14 @@ import { Vector3 } from "three";
 const SCENE = {
   numShapes: 15,
   ambientIntensity: 0.5,
-  pointIntensity: 100,
+  pointIntensity: 5,
   lightPosition: new Vector3(10, 10, 10),
   RADIUS: 0.75,
   HEIGHT: 0.5,
   SEGMENTS: 6,
-  MAX_X: 15,
+  MAX_X: 10,
   MIN_X: -15,
+  SCALE: 1,
 };
 
 export { SCENE };
